025990566f5a307e887e83cfb347027d4547537e
Mahmoud Khattab <khattm@mcmaster.ca>
2018-04-04 01:12:40 -0400

    Seperate loop and fn parsing

--- read_in_functions.py
+++ read_in_functions.py
@@ -15,7 +15,12 @@ import sys
 import re
 import random
 
+genvarcnt = -1
+
+lnidx = 0
+
 def genRandomVarName():
+	global genvarcnt
 	varName = "Var_"
 	for i in range(5):
 		uppercase = random.randrange(2)
@@ -24,13 +29,17 @@ def genRandomVarName():
 			varName += chr(65 + letter)
 		else:
 			varName += chr(97 + letter)
-	return varName + str(random.randrange(100))
+	genvarcnt += 1
+	return varName + '_' + str(genvarcnt)
 
 
-def replace_params(func_def, call_params, indentation):
+def replace_params(func_def, call_params, indentation, vars_in_scope):
 	fname = func_def["fname"]
 	fparams = func_def["params"]
 	fcode = func_def["fcode"]
+
+	print("VARSINSCOPE: ")
+	print(vars_in_scope)
 
 	#TODO: replace literal values in call_params with variable names
 	returnVar = genRandomVarName()
@@ -52,6 +61,8 @@ def replace_params(func_def, call_params, indentation):
 					if not func_call_found: # This is a variable
 						if ident in fparams: # This variable was passed in
 							line_inlined += call_params[fparams.index(ident)]
+						elif ident in vars_in_scope: 
+							line_inlined += genRandomVarName()
 						else:
 							line_inlined += ident
 					else:
@@ -69,7 +80,80 @@ def replace_params(func_def, call_params, indentation):
 	return fcode_inlined, returnVar
 
 
-def parser(read_data, funcs = {}, inner_loop = 0, scope_indentation = ""):
+def func_def(line_num, def_found, read_data, funcs):
+	#print("FOUND A DEF!!")
+	indent = len(def_found.group(1))
+	fname = def_found.group(2)
+	params = def_found.group(3).replace(" ","").replace("\t","").split(",")
+	#print(len(def_found.group(4)))
+	if len(def_found.group(4)) > 0 and not re.match("^\s*$", def_found.group(4)):
+		body = [def_found.group(1) + def_found.group(4) + "\n"]
+	else:
+		body = []
+		while line_num < len(read_data)-1:
+			line_num += 1
+			#print(read_data[line_num])
+			if re.match("^\s*\n$", read_data[line_num]): #Skip empty lines 
+				continue
+			if re.match("^\s*#.*$", read_data[line_num]): #Skip comment lines
+				continue	
+			
+			m2 = re.search("^(\t*).*$",read_data[line_num])
+			indent2 = len(m2.group(1))
+
+			if indent2 > indent: #All lines in the functions will be indented more than the function def line
+				indentless = re.match("^(\t*)(.*\n)$",read_data[line_num])
+				print("INDENTLESS: " + indentless.group(2))
+				tabs = ""
+				for i in range(indent2-indent-1):
+					tabs += "\t"
+				body.append(tabs + indentless.group(2))
+			else: # We are outside the function (i.e. done)
+				line_num -= 1 # Corrective
+				break
+
+	f_code = {"fname": fname, "params": params, "fcode": body}
+
+	return f_code, line_num
+
+def loops(line_num, loop_found, read_data, funcs):
+	#print("FOUND A LOOP!")
+	#print(read_data[line_num])
+
+	indent = len(loop_found.group(1))
+	first_line = loop_found.group(2)
+
+	# print (len(first_line))
+	# print(first_line)
+	if len(first_line) > 0 and not re.match("^\s*$", first_line):
+		body = [loop_found.group(1) + first_line + "\n"]
+	else:
+		body = []
+		while line_num < len(read_data)-1:
+			line_num += 1
+			#print(read_data[line_num])
+			if re.match("^\s*\n$", read_data[line_num]): #Skip empty lines 
+				continue
+			if re.match("^\s*#.*$", read_data[line_num]): #Skip comment lines
+				continue	
+			
+			m2 = re.search("^(\t*).*$",read_data[line_num])
+			indent2 = len(m2.group(1))
+
+			if indent2 > indent: #All lines in the functions will be indented more than the function def line
+				indentless = re.match("^(\t*)(.*\n)$",read_data[line_num])
+				tabs = ""
+				for i in range(indent2-indent-1):
+					tabs += "\t"
+				body.append(tabs + indentless.group(2))
+			else: # We are outside the function (i.e. done)
+				line_num -= 1 # Corrective
+				break
+
+	return body, line_num
+
+
+def parser(read_data, funcs = {}, vars_in_scope = [], inner_loop = 0, scope_indentation = ""):
 	line_num = 0	#init counter
 	#funcs = {}	#init list for storing functions
 
@@ -84,47 +168,14 @@ def parser(read_data, funcs = {}, inner_loop = 0, scope_indentation = ""):
 		while_loop_found = re.search("^(\t*)while\s+.*:\s*(.*)\n$", read_data[line_num])
 		
 		if def_found:
-			#print("FOUND A DEF!!")
-			indent = len(def_found.group(1))
-			indentation = def_found.group(1)
-			fname = def_found.group(2)
-			params = def_found.group(3).replace(" ","").replace("\t","").split(",")
-			
-			#print(len(def_found.group(4)))
+
 			opt_code.append(read_data[line_num][:-len(def_found.group(4))-1] + "\n")
-
-			if len(def_found.group(4)) > 0 and not re.match("^\s*$", def_found.group(4)):
-				body = [def_found.group(1) + def_found.group(4) + "\n"]
-			else:
-				body = []
-				while line_num < len(read_data)-1:
-					line_num += 1
-					#print(read_data[line_num])
-					if re.match("^\s*\n$", read_data[line_num]): #Skip empty lines 
-						continue
-					if re.match("^\s*#.*$", read_data[line_num]): #Skip comment lines
-						continue	
-					
-					m2 = re.search("^(\t*).*$",read_data[line_num])
-					indent2 = len(m2.group(1))
-
-					if indent2 > indent: #All lines in the functions will be indented more than the function def line
-						indentless = re.match("^(\t*)(.*\n)$",read_data[line_num])
-						print("INDENTLESS: " + indentless.group(2))
-						tabs = ""
-						for i in range(indent2-indent-1):
-							tabs += "\t"
-						body.append(tabs + indentless.group(2))
-					else: # We are outside the function (i.e. done)
-						line_num -= 1 # Corrective
-						break
-
-			f_code = {"fname": fname, "params": params, "fcode": body}
-			funcs[fname] = f_code
+			f_code, line_num = func_def(line_num, def_found, read_data, funcs)
+			funcs[f_code["fname"]] = f_code
 
 			#print(body)
 
-			opt_body = parser(body, funcs, 0, indentation + "\t") # We want to parse body of function recursively
+			opt_body = parser(f_code["fcode"], funcs, vars_in_scope, 0, def_found.group(1) + "\t") # We want to parse body of function recursively
 								# Funcs we have already found will be available to funcs defined inside current function
 			opt_code = opt_code + opt_body
 		elif for_loop_found or while_loop_found:
@@ -134,50 +185,28 @@ def parser(read_data, funcs = {}, inner_loop = 0, scope_indentation = ""):
 			inner_loop = 0 # If we just found a loop, this can't the body of an inner loop
 
 			if for_loop_found:
-				indent = len(for_loop_found.group(1))
-				indentation = for_loop_found.group(1)
-				first_line = for_loop_found.group(2)
-				opt_code.append(read_data[line_num][:-len(for_loop_found.group(2))-1] + "\n")
+				loop_found = for_loop_found
 			else:
-				indent = len(while_loop_found.group(1))
-				indentation = while_loop_found.group(1)
-				first_line = while_loop_found.group(2)
-				opt_code.append(read_data[line_num][:-len(while_loop_found.group(2))-1] + "\n")
+				loop_found = while_loop_found
 
+			opt_code.append(read_data[line_num][:-len(loop_found.group(2))-1] + "\n")
 			# print (len(first_line))
 			# print(first_line)
-			if len(first_line) > 0 and not re.match("^\s*$", first_line):
-				body = [indentation + first_line + "\n"]
-			else:
-				body = []
-				while line_num < len(read_data)-1:
-					line_num += 1
-					#print(read_data[line_num])
-					if re.match("^\s*\n$", read_data[line_num]): #Skip empty lines 
-						continue
-					if re.match("^\s*#.*$", read_data[line_num]): #Skip comment lines
-						continue	
-					
-					m2 = re.search("^(\t*).*$",read_data[line_num])
-					indent2 = len(m2.group(1))
 
-					if indent2 > indent: #All lines in the functions will be indented more than the function def line
-						indentless = re.match("^(\t*)(.*\n)$",read_data[line_num])
-						tabs = ""
-						for i in range(indent2-indent-1):
-							tabs += "\t"
-						body.append(tabs + indentless.group(2))
-					else: # We are outside the function (i.e. done)
-						line_num -= 1 # Corrective
-						break
+			body, line_num = loops(line_num, loop_found, read_data, funcs)
 
-			#print(body)
-			opt_body = parser(body, funcs, 1, indentation+"\t") # We want to parse body of function recursively
+			opt_body = parser(body, funcs, vars_in_scope, 1, loop_found.group(1)+"\t") # We want to parse body of function recursively
 									# Set flag inner_loop to say 
 			opt_code = opt_code + opt_body
 		else:
 			print("HERE:" + read_data[line_num])
 			opt_code.append(read_data[line_num])
+			# Keep a dictionary of variables in this scope
+			assign_found = re.search("^\t*(.*)=.*$", read_data[line_num])
+			if assign_found:
+				vars_assigned = assign_found.group(1).replace(" ","").replace("\t","").split(",")
+				vars_in_scope = vars_in_scope + vars_assigned
+
 		line_num += 1
 		
 	print(opt_code)
@@ -207,14 +236,20 @@ def parser(read_data, funcs = {}, inner_loop = 0, scope_indentation = ""):
 						func_call_found = re.search("^\s*\(", ident_found.string[ident_found.end():])
 						if func_call_found: # This is a func call
 							params_found = re.search("^(.*)\)", func_call_found.string[func_call_found.end():])
-							if ident in funcs.keys(): # If this function is in funcs 
+							if params_found:
 								params = params_found.group(1).replace(" ","").replace("\t","").split(",")
-								inline_it, returnVar = replace_params(funcs[ident], params, scope_indentation + indentation)
+								params_end = params_found.end()
+							else:
+								params = ""
+								params_end = 1
+							if ident in funcs.keys(): # If this function is in funcs 
+
+								inline_it, returnVar = replace_params(funcs[ident], params, scope_indentation + indentation, vars_in_scope)
 								inlined_code = inlined_code + inline_it
 								line_rewrite += returnVar
 							else:
 								line_rewrite += ident + "(" + params_found.group(1) + ")"
-							i = i + params_found.end() + func_call_found.end() + ident_found.end()
+							i = i + params_end + func_call_found.end() + ident_found.end()
 						else:
 							line_rewrite += ident
 							i = i + ident_found.end()
@@ -247,7 +282,7 @@ def main():
 
 	opt_code = parser(read_data) 
 
-	print(opt_code)
+	#print(opt_code)
 	new_file = open('generated.py', 'w')
 	for item in opt_code:
 		#print(opt_code)
